840be68c96b7657e985825e802704a31cdad0af2
alvinhill <rainfirex@att.net>
2023-07-02 16:54:48 -0700

    UPDATE REPOSITORY

--- HOLD/CANVAS/JS/main.js
+++
@@ -1,5 +0,0 @@
-// THIS IS STEP 238 //
-const myCanvas = document.getElementById("myCanvas");
-const ctx = myCanvas.getContext("2d");
-
-ctx.fillRect(20, 20, 150, 100);
--- HOLD/ERROR/JS/main.js
+++
@@ -1,7 +0,0 @@
-// THIS IS STEP 123 //
-
-document.write (5 > 2 && 10 > 4)
-
-document.write(1 > 2 || 2 > 4)
-
-
